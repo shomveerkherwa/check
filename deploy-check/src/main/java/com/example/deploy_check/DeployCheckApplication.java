f8437dbeda7f029d48bb6bc2173a8fb4ad6c7328
shomveer.kherwa <shomveer.kherwa@gmail.com>
2025-05-16 14:18:44 +0530

    Actions pipeline for ecr

---
+++ deploy-check/src/main/java/com/example/deploy_check/DeployCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.deploy_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployCheckApplication.class, args);
+	}
+
+}
---
+++ deploy-check/src/test/java/com/example/deploy_check/DeployCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deploy_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
